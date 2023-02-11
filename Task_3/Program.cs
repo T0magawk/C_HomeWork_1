aadfe0c529908ba2f09d46f6029b4f8d338e2be6
Александр Шушпанов <aleksandrshushpanov@MacBook-Air-Aleksandr.local>
2023-02-11 17:32:14 +0300

    Задача 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите любое число для проверки");
+int n = int.Parse(Console.ReadLine ()!);
+// int a = 0;
+int count = 2;
+
+while(count <= n)
+{
+    Console.WriteLine(count);
+    count = count + 2;
+}
+//if (n % 2 == 0)
+//{
+//    Console.WriteLine("Это число четное, все его четные:");
+  //  a = n;
+  //  while (count > a)
+  //  {
+  //      Console.WriteLine(count );
+  //      count = count + 2;
+  //  }    
+  //  Console.WriteLine(count );
+//}
+
+//else
+//{
+//    Console.WriteLine("Это число нечетное, все его четные:");
+//    a = n-1;
+//    Console.WriteLine(count );
+//    
+//    while (count > a)
+//    {   
+//        count = count + 2;
+//        Console.WriteLine(count );
+//        
+//    }    
+//
+//}
+ //Console.WriteLine(a);
